a649d4a299cca72a409adaec66aca913481a4842
qsoyq <github@19940731.xyz>
2026-02-14 03:29:27 +0800

    chore: reformat github rss routers and increase cache ttl

--- rssapi/applications/rss/routers/github/commit.py
+++ rssapi/applications/rss/routers/github/commit.py
@@ -6,28 +6,27 @@ from asyncache import cached
 from fastapi import APIRouter, HTTPException, Path, Query, Request
 
 from rssapi.applications.rss.schemas.github import AuthorSchema
-from rssapi.applications.rss.schemas.github.commits import (CommitItemSchema,
-                                                            CommitSchema)
+from rssapi.applications.rss.schemas.github.commits import CommitItemSchema, CommitSchema
 from rssapi.applications.rss.schemas.rss.jsonfeed import JSONFeed, JSONFeedItem
 from rssapi.core.responses import PrettyJSONResponse
 from rssapi.utils.cache import RandomTTLCache
 
-router = APIRouter(tags=['RSS'], prefix='/rss/github/commits')
+router = APIRouter(tags=["RSS"], prefix="/rss/github/commits")
 
 logger = logging.getLogger(__file__)
 
 
 @router.get(
-    '/repos/{owner}/{repo}',
-    summary='Github Repo Commits RSS',
+    "/repos/{owner}/{repo}",
+    summary="Github Repo Commits RSS",
     response_model=JSONFeed,
     response_class=PrettyJSONResponse,
 )
 async def commits_list(
     req: Request,
-    token: str | None = Query(None, description='Github API Token'),
-    owner: str = Path(..., description='Github Repo Owner'),
-    repo: str = Path(..., description='Github Repo Name'),
+    token: str | None = Query(None, description="Github API Token"),
+    owner: str = Path(..., description="Github Repo Owner"),
+    repo: str = Path(..., description="Github Repo Name"),
     per_page: int = Query(30, ge=1, le=100),
     page: int = Query(1, ge=1),
 ):
@@ -37,34 +36,34 @@ async def commits_list(
     host = req.url.hostname
     items: list[JSONFeedItem] = await fetch_feeds(owner, repo, token, per_page, page)
     feed = {
-        'version': 'https://jsonfeed.org/version/1',
-        'title': f'{owner}/{repo}',
-        'description': '',
-        'home_page_url': f'https://github.com/{owner}/{repo}',
-        'feed_url': f'{req.url.scheme}://{host}{req.url.path}?{req.url.query}',
-        'icon': f'https://github.com/{owner}.png',
-        'favicon': f'https://github.com/{owner}.png',
-        'items': items,
+        "version": "https://jsonfeed.org/version/1",
+        "title": f"{owner}/{repo}",
+        "description": "",
+        "home_page_url": f"https://github.com/{owner}/{repo}",
+        "feed_url": f"{req.url.scheme}://{host}{req.url.path}?{req.url.query}",
+        "icon": f"https://github.com/{owner}.png",
+        "favicon": f"https://github.com/{owner}.png",
+        "items": items,
     }
 
     return feed
 
 
-@cached(RandomTTLCache(4096, 300))
+@cached(RandomTTLCache(4096, 1800))
 async def fetch_feeds(owner: str, repo: str, token: str | None, per_page: int, page: int) -> list[JSONFeedItem]:
     items = []
 
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
     headers = {
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
     }
     if token is not None:
-        headers['Authorization'] = f'Bearer {token}'
+        headers["Authorization"] = f"Bearer {token}"
 
     params = {
-        'per_page': per_page,
-        'page': page,
+        "per_page": per_page,
+        "page": page,
     }
     async with httpx.AsyncClient(headers=headers) as client:
         res = await client.get(url, params=params)
@@ -78,16 +77,16 @@ async def fetch_feeds(owner: str, repo: str, token: str | None, per_page: int, p
         assert commit.commit.author
         assert commit.author
         payload: dict[str, Any] = {
-            'id': f'github-commits-{owner}-{repo}-{commit.sha}',
-            'url': commit.html_url,
-            'title': commit.commit.message,
-            'content_text': '',
-            'date_published': commit.commit.author.date,
-            'date_modified': commit.commit.author.date,
-            'author': {
-                'url': commit.author.html_url,
-                'name': commit.author.login,
-                'avatar': commit.author.avatar_url,
+            "id": f"github-commits-{owner}-{repo}-{commit.sha}",
+            "url": commit.html_url,
+            "title": commit.commit.message,
+            "content_text": "",
+            "date_published": commit.commit.author.date,
+            "date_modified": commit.commit.author.date,
+            "author": {
+                "url": commit.author.html_url,
+                "name": commit.author.login,
+                "avatar": commit.author.avatar_url,
             },
         }
         items.append(JSONFeedItem(**payload))
